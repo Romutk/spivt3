6f61421f36389dc3c40c64d29c5dfb5fe477913b
Рома <newangel88@mail.ru>
2016-03-25 11:41:22 +0400

    Убрал настройки Диапазона безопасности MLS/MCS, так как они не нужны

--- 1.sh
+++ 1.sh
@@ -4,7 +4,7 @@ useradd webmaster
 passwd webmaster
 
 
-semanage login -a -s staff_u -r s0-s0:c0.c1023 webmaster 
+semanage login -a -s staff_u  webmaster 
 semanage user -m -R 'staff_r webadm_r system_r' staff_u
 echo 'webmaster ALL=(ALL) TYPE=webadm_t ROLE=webadm_r ALL' > /etc/sudoers.d/webmaster  
 
